6b1b46bfde530fb5d395dc7e0f9fad1e2f6c5a29
dhruvidesai20 <71321899+dhruvidesai20@users.noreply.github.com>
2020-10-13 22:41:07 +0530

    Project

---
+++ main.js
@@ -0,0 +1,65 @@
+var p1= document.getElementById("page1");
+var p2= document.getElementById("page2");
+
+var startingX;
+
+function p1Start(evt){
+  startingX= evt.touches[0].clientX;
+}
+
+function p1Move(evt){
+  var touch= evt.touches[0];
+  var change= startingX - touch.clientX;
+  if (change<0) {
+    return;
+  }
+  p1.style.left= '-' + change + 'px';
+  p2.style.display= 'block';
+  p2.style.left= (screen.width - change) + 'px';
+  evt.preventDefault();
+}
+
+function p1End(evt){
+  var change= startingX - evt.changedTouches[0].clientX;
+  var diff= screen.width / 3;
+  if (change<diff) {
+    p1.style.left= 0;
+    p2.style.left= '100%';
+    p2.style.display= 'none';
+  }
+  else{
+    p1.style.left= '-100%';
+    p2.style.left= 0;
+    p2.style.display= 'block';
+  }
+}
+
+function p2Start(evt){
+  startingX= evt.touches[0].clientX;
+}
+
+function p2Move(evt){
+  var touch= evt.touches[0];
+  var change=  touch.clientX - startingX;
+  if (change<0) {
+    return;
+  }
+  p1.style.left= change + 'px';
+  p1.style.display= 'block';
+  p2.style.left= (change - screen.width) + 'px';
+  evt.preventDefault();
+}
+
+function p2End(evt){
+  var change= evt.changedTouches[0].clientX - startingX;
+  var diff= screen.width / 4;
+  if (change<diff) {
+    p2.style.left= 0;
+    p1.style.left= '-100%';
+    p1.style.display= 'none';
+  }
+  else{
+    p2.style.left= '100%';
+    p1.style.left= 0;
+  }
+}
