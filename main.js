61f507ff74aaae82582cfb715c7d57ed22d372a9
dhruvidesai20 <71321899+dhruvidesai20@users.noreply.github.com>
2020-10-14 15:33:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,49 +48,53 @@ function p2Move(evt){
   var touch= evt.touches[0];
   var change=  touch.clientX - startingX;
   if (change<0) {
+    var touch= evt.touches[0];
+    var change= startingX - touch.clientX;
+    if (change<0) {
+      return;
+    }
+    p1.style.display='none;'
+    p2.style.left= '-' + change + 'px';
+    p3.style.display= 'block';
+    p3.style.left= (screen.width - change) + 'px';
+    evt.preventDefault();
     return;
   }
-  p1.style.display= 'block';
-  p1.style.left= (change - screen.width) + 'px';
-  p2.style.left= change + 'px';
-  evt.preventDefault();
-  var touch= evt.touches[0];
-  var change= startingX - touch.clientX;
-  if (change<0) {
-    return;
-  }
-  p2.style.left= '-' + change + 'px';
-  p3.style.display= 'block';
-  p3.style.left= (screen.width - change) + 'px';
-  evt.preventDefault();
+    p1.style.display= 'block';
+    p1.style.left= (change - screen.width) + 'px';
+    p2.style.left= change + 'px';
+    evt.preventDefault();
 }
 
 function p2End(evt){
   var change= evt.changedTouches[0].clientX - startingX;
-  var diff= screen.width / 8;
+  var diff= screen.width / 4;
   if (change<diff) {
     p1.style.left= '-100%';
     p1.style.display= 'none';
     p2.style.left= 0;
   }
-  else{
-    p1.style.transition= 'all .3s';
-    p2.style.transition= 'all .3s';
-    p1.style.left= 0;
-    p2.style.left= '100%';
+if (change>0) {
+  p1.style.transition= 'all .3s';
+  p2.style.transition= 'all .3s';
+  p1.style.left= 0;
+  p2.style.left= '100%';
+  p2.style.display='none';
   }
-  var change= startingX - evt.changedTouches[0].clientX;
-  var diff= screen.width / 3;
-  if (change<diff) {
-    p2.style.left= 0;
-    p3.style.left= '100%';
-    p3.style.display= 'none';
-  }else{
-    p2.style.transition= 'all .3s';
-    p3.style.transition= 'all .3s';
-    p2.style.left= '-100%';
-    p3.style.left= 0;
-    p3.style.display= 'block';
+else {
+    var change= startingX - evt.changedTouches[0].clientX;
+    var diff= screen.width / 3;
+    if (change<diff) {
+      p2.style.left= 0;
+      p3.style.left= '100%';
+      p3.style.display= 'none';
+    }else{
+      p2.style.transition= 'all .3s';
+      p3.style.transition= 'all .3s';
+      p2.style.left= '-100%';
+      p3.style.left= 0;
+      p3.style.display= 'block';
+  }
 }
 }
 
